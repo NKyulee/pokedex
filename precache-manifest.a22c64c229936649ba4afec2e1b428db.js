c1fd4050398bd2fc8b80f1af8d8063b3eb14dbc2
Kyulee <yunguyen94@gmail.com>
2020-06-07 22:44:14 +0200

    Updates

---
+++ precache-manifest.a22c64c229936649ba4afec2e1b428db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a2ea63471b924fe56fcfa311b1ad02",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0ecda36c5bdb90591d01",
+    "url": "/pokedex/static/css/main.3609af5d.chunk.css"
+  },
+  {
+    "revision": "312b0a490f0c1bd1ec77",
+    "url": "/pokedex/static/js/2.6d62aeb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.6d62aeb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecda36c5bdb90591d01",
+    "url": "/pokedex/static/js/main.1af299f3.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/pokedex/static/media/sprites.6a398990.png"
+  }
+]);
